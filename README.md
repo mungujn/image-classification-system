e9bc0006ffde85f3c9873bcf536fd37dc31d722f
Mungujakisa Nickson <nicksonmjk@gmail.com>
2018-11-18 16:20:37 +0300

    edited readme

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Frontend simulation
+# Image categorizer. An image classification application
 
-This script interacts with the two micro-services 'classifier' and 'file-handler', serving to demonstrate the capability of the system I have built to answer question 2 of the python assesment.
+An app that categorizes images in a drop box folder. To help understand the app consider this scenario: You have 1000 photos in a drop box folder. The photos are of 5 types of items. You would like to generate 5 folders, each having all the photos of a particular item type.
+
+## System overview
+
+The system utilizes two services, the [image-classifier](https://github.com/mungujn/image-classifier) and the [downloader-uploader](https://github.com/mungujn/downloader-uploader) service .
+
+The frontend described in this repo is a script that interacts with the two micro-services, serving to demonstrate usage of the system. It invokes the downloader-uploader first to dowload images from a dropbox folder called 'all'. It then invokes the image-classifier service to classify these images and move them to individual folders. Finally the frontend invokes the downloader-uploader service again to upload the classified images back up to seperate folders in dropbox.
+
+The [image-classifier](https://github.com/mungujn/image-classifier) and [downloader-uploader](https://github.com/mungujn/downloader-uploader) repos each have a READMEs that explain their design and functionality in more detail.
+
+## Running the system
+
+1. Generate a dropbox access token and set it as an environment variable called ACCESS_TOKEN
+2. Define a SERVICE_KEY environment varible which serves as an extra layer of security
+3. Create a folder in dropbox called 'All'
+4. Place the images that you want to classify in that folder
+5. Install the requirements defined in each services' requirements.txt file
+6. Start the [downloader-uploader](https://github.com/mungujn/downloader-uploader) service
+7. Start the [image-classifier](https://github.com/mungujn/image-classifier) service
+8. Run main.py and pass in the classes you want to sort your uploaded images by
+   e.g 'python main.py --classes cat dog truck' will sort for cats, dogs and trucks
+9. The system will start the classification and update you on its progress along the way
+
+## Testing
+
+Refer to the individual service repos. I have included each services' build status below;
+
+### Classifier service
+
+[![Build Status](https://travis-ci.com/mungujn/image-classifier.svg?branch=master)](https://travis-ci.com/mungujn/image-classifier)
+[![codecov](https://codecov.io/gh/mungujn/image-classifier/branch/master/graph/badge.svg)](https://codecov.io/gh/mungujn/image-classifier)
+
+### Downloader, uploader service
+
+[![Build Status](https://travis-ci.com/mungujn/downloader-uploader.svg?branch=master)](https://travis-ci.com/mungujn/downloader-uploader)
+[![codecov](https://codecov.io/gh/mungujn/downloader-uploader/branch/master/graph/badge.svg)](https://codecov.io/gh/mungujn/downloader-uploader)
--- main.py
+++ main.py
@@ -1,42 +1,120 @@
 import requests
+import os
 import argparse
+import time
+from dotenv import load_dotenv
+load_dotenv()
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+AUTHORIZATION = os.environ['SERVICE_KEY']
 
 parser = argparse.ArgumentParser(description='System frontend')
 parser.add_argument(
     "--classes",
     nargs="*",  # expects ≥ 0 arguments
     type=str,
-    # default list if no arg value
+    # default list if no classes are given
     default=['cat', 'dog', 'horse', 'car', 'truck'],
 )
 
-job = None
+job = {}
+percentage = 0
+classes = []
 
 
-def startDownloadJob():
-    r = requests.post('http://localhost:8081/download-job')
-    job = r.json()
-    print(f'Job {job} started')
+def startJob(job_type):
+    """Start a job of job_type
+
+    Arguments:
+        job_type {string} -- [type of job]
+    """
+    global job
+    job = {}
+    headers = {'Token': ACCESS_TOKEN, 'Authorization': AUTHORIZATION}
+
+    if job_type == 'download':
+        url = 'http://localhost:8081/download-job'
+        r = requests.post(url, headers=headers)
+        job = r.json()
+    elif job_type == 'classification':
+        url = 'http://localhost:8080/classification-job'
+        r = requests.post(url, json={'classes': classes})
+        job = r.json()
+    elif job_type == 'upload':
+        url = 'http://localhost:8081/upload-job'
+        r = requests.post(url, json={'classes': classes}, headers=headers)
+        jobs = r.json()
+
+        if len(jobs) == 0:
+            job = {'id': None, 'percentage': 100, 'complete': True}
+        else:
+            job = jobs[len(jobs) - 1]
+
+    job_id = job['id']
+    print(f'{job_type} job {job_id} has started on server')
 
 
-def checkStatusOfDownloadJob():
-    pass
+def checkStatusOfJob(job_type):
+    """Start a job of job_type
+
+    Arguments:
+        job_type {string} -- [type of job]
+    """
+    global job, percentage
+    job_id = job['id']
+    headers = {'Token': ACCESS_TOKEN, 'Authorization': AUTHORIZATION}
+
+    if job_type == 'download':
+        url = f'http://localhost:8081/download-job/{job_id}'
+        r = requests.get(url, headers=headers)
+        job = r.json()
+    elif job_type == 'classification':
+        url = f'http://localhost:8080/classification-job/{job_id}'
+        r = requests.get(url, headers=headers)
+        job = r.json()
+    elif job_type == 'upload':
+        url = f'http://localhost:8081/upload-job/{job_id}'
+        r = requests.get(url, headers=headers)
+        job = r.json()
+
+    if job['percentage'] > percentage:
+        percentage = job['percentage']
+        print(
+            f'{job_type} job {job_id} is {percentage} percent complete')
 
 
-def startClassificationJob():
-    pass
+def download():
+    """Download images"""
+    print('*************************************')
+    print(f'Download of images has started')
+    startJob('download')
+    while not job['complete']:
+        checkStatusOfJob('download')
+        # a better strategy here would be to calculate the estimated completion time and check then
+        time.sleep(2)
+    print(f'Download of images has completed')
 
 
-def checkStatusOfClassificationJob():
-    pass
+def classify():
+    """Classify downloaded images"""
+    print('*************************************')
+    print(f'Classification of {classes} has started')
+    startJob('classification')
+    while not job['complete']:
+        checkStatusOfJob('classification')
+        time.sleep(2)
+    print(f'Classification of images has completed')
 
 
-def startUploadJob():
-    pass
-
-
-def checkStatusOfUploadJob():
-    pass
+def upload():
+    """Upload images"""
+    print('*************************************')
+    print(f'Upload of {classes} has started')
+    startJob('upload')
+    while not job['complete']:
+        checkStatusOfJob('upload')
+        time.sleep(2)
+    print(f'Upload of images has completed')
+    print('*************************************')
 
 
 if __name__ == '__main__':
@@ -44,7 +122,23 @@ if __name__ == '__main__':
     """
     args = parser.parse_args()
     classes = args.classes
-    print('Classifying images of the following classes: ', classes)
-    startDownloadJob()
+    retry = True
 
+    while retry:
+        try:
+            download()
 
+            classify()
+
+            upload()
+            retry = False
+        except Exception as error:
+            print('\nThe following error has occured \n')
+            print('*************************************')
+            print(error)
+            print('*************************************')
+            retry = input('Do you want to retry? yes or no: ')
+            if retry == 'yes' or retry == 'y':
+                retry = True
+            else:
+                retry = False
